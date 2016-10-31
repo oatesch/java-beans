11932116eccfa2f7ce5e74f1afc12784b62e462d
Garth Oates <garth.oates@gmail.com>
2016-10-31 12:33:33 +0000

    Work up to Age of Majority

--- 2013-OOProgrammingWithJava-PART1/week1-014.PositiveValue/src/PositiveValue.java
+++ 2013-OOProgrammingWithJava-PART1/week1-014.PositiveValue/src/PositiveValue.java
@@ -5,7 +5,17 @@ public class PositiveValue {
 
     public static void main(String[] args) {
         Scanner reader = new Scanner(System.in);
-
         // Type your program here:
+        
+        System.out.print("Type a number: ");
+        int a = Integer.parseInt(reader.nextLine());
+        if (a > 0) {
+            System.out.println("The number is positive.");
+        } else {
+            System.out.println("The number is not positive.");
+        }
+
+        
+        
     }
 }
